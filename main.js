ea66f8d13330697a4540f9985fcd9925712ef4d3
Krzysztof Kirsz <k.kirsz@gmail.com>
2017-07-12 22:22:57 +0200

    Add dev quotes and export interface

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var http = require('http');
-var quoter = require('./quotes');
+var quotor = require('./quotes');
 
-http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end("Random quote for you:<br><h1>" + quoter() + "</h1>");
-}).listen(8081);
+var startServer = function(port){
+  http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end("Random quote for you:<br><h1>" + quotor() + "</h1>");
+  }).listen(port);
+  console.log("Server running @"+port);
+}
 
-console.log("Server running @8081");
+quoter.startServer = startServer;
+
+module.exports = quotor;
