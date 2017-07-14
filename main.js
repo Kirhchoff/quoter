e64b2894f5a0ef6a29c2d0b6cb34094f4fab0d85
Kirhchoff <k.kirsz@gmail.com>
2017-07-14 21:37:51 +0200

    add trivial api and naive test

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var http = require('http');
+var url = require('url');
 var quotor = require('./quotes');
 
 var startServer = function(port){
   http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end("Random quote for you:<br><h1>" + quotor() + "</h1>");
+    var pathname = url.parse(request.url).pathname;
+    console.log(pathname);
+    if(pathname == "/quote"){
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end(quotor());
+    }
+    else{
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end("Random quote for you:<br><h1>" + quotor() + "</h1>");
+    }
   }).listen(port);
   console.log("Server running @"+port);
 }
 
-quoter.startServer = startServer;
+quotor.startServer = startServer;
 
 module.exports = quotor;
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var out = require('../main');
+
+console.log(out());
+console.log(out.toString());
+out.startServer(8081);
